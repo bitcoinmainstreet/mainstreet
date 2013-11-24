f3f8d459e0f0ac91c6284728949e272a043070d0
Bitcoin Mainstreet <bitcoinmainstreet@safe-mail.net>
2013-11-24 00:50:30 +0000

    cleanup

--- application/controllers/stocks.php
+++ application/controllers/stocks.php
@@ -1,37 +1,24 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+require_once('controller.php');
+class Stocks extends Controller {
+  public function __construct(){
+    parent::__construct();
+    $this->out['app_name']='Stocks';
+  }
 
-class Stocks extends CI_Controller_Extender {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
-	
-	public function stock(){
-	       echo 'stock';
-	}
-	public function buy(){
-	       echo 'buy';
-	}
+  public function index()
+  {
+    $this->load->view('welcome_message');
+  }
+  
+  public function stock(){
+    echo 'stock';
+    $this->render();
+  }
+  public function buy(){
+    echo 'buy';
+  }
+}  
 
 
 
-}
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
