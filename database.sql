93da4a9480b78aaa5f7032ea6fb5be1234d50624
Bitcoin Mainstreet <bitcoinmainstreet@safe-mail.net>
2013-12-07 16:09:32 +0000

    added database dump (currently contains just user) 2013-12-07

---
+++ database.sql
@@ -0,0 +1,11 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
